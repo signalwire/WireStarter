393115b0cfa3132d7526c3a81f01f4e039f42621
Brian West <brian@signalwire.com>
2022-10-23 10:33:55 -0500

    simple node example

---
+++ examples/node/start.js
@@ -0,0 +1,24 @@
+const { Voice } = require('@signalwire/realtime-api')
+
+const projectId = process.env.PROJECT_ID;
+const token = process.env.API_TOKEN;
+
+const client = new Voice.Client({
+ project: projectId,
+ token: token,
+ contexts: ["home","office"],
+});
+
+client.on("call.received", async (call) => {
+ console.log("Got call", call.from, call.to);
+
+ try {
+  await call.answer();
+  console.log("Inbound call answered");
+  const playback = await call.playTTS({ text: "Welcome to SignalWire!" });
+  await playback.waitForEnded();
+  await call.hangup()
+ } catch (error) {
+  console.error("Error answering inbound call", error);
+ }
+});
