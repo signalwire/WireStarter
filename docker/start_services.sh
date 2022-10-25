d3f14e9869dd569c6eb5ba9a102fabdcaa31fb20
Brian West <brian@signalwire.com>
2022-10-25 15:29:21 -0500

    typo and works if you define the auth token

--- docker/start_services.sh
+++ docker/start_services.sh
@@ -19,3 +19,7 @@ if [ ! -z $NGROK_URL ]; then
   python3 /usr/lib/cgi-bin/update_laml_bins.py $NGROK_URL
 fi
 
+#LocalToNet                                                                                                                                                                                                                                                                                        
+if [ ! -z $LOCALTONET_AUTH_TOKEN ]; then
+    /usr/bin/screen -dmS localtonet /usr/bin/localtonet authtoken $LOCALTONET_AUTH_TOKEN 2>&1 > /root/localtonet.log
+fi
