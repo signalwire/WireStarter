e7f75140f69bc178c7224151ea5d93cd7ac357df
Len <Len-PGH@users.noreply.github.com>
2022-10-25 17:35:25 -0400

    Update bat

--- docker/windows.bat
+++ docker/windows.bat
@@ -2,24 +2,27 @@
 
 title ~#~#~#~#~#~#~Swish~#~#~#~#~#~#~
 :start
-set /p sig_space="What is your Signalwire space "
 
+if exist .env ( 
+ echo "Remove .env to run again. Press the only key to exit"
+ pause  exit
+) else (
+
+set /p sig_space="What is your Signalwire space "
 set /p proj_id= "What is your Signalwire project ID "
 set /p api_token= "What is your Signalwire REST API token "
 set /p ngrok_token= "What is your NGROK Token "
+set /p visual_editor= "What editor to use? nano, vim, emacs "
+set /p localtonet_api_token= "What is your NGROK Token "
+
+echo SIGNALWIRE_SPACE=%sig_space% > .env
+echo PROJECT_ID=%proj_id% >> .env
+echo REST_API_TOKEN=%proj_id% >> .env
+echo NGROK_TOKEN=%ngrok_token% >> .env
+echo VISUAL=%visual_editor% >> .env
+echo LOCALTONET_API_TOKEN=%localtonet_api_token% >> .env
 
 
+docker "run" "-it" "wirestarter"
 
-REM build:
-
-REM docker build --build-arg SIGNALWIRE_SPACE=%sig_space% --build-arg PROJECT_ID=%proj_id% --build-arg REST_API_TOKEN=%api_token% --build-arg NGROK_TOKEN=%ngrok_token% -t signalwire-getting-started %CD%docker\%CD%
-
-docker build --build-arg SIGNALWIRE_SPACE=%sig_space% --build-arg PROJECT_ID=%proj_id% --build-arg REST_API_TOKEN=%api_token% --build-arg NGROK_TOKEN=%ngrok_token% -t signalwire-getting-started  -f Dockerfile .
-
-REM run:
-
-docker "run" "-it" "signalwire-getting-started"
-
-REM up: "build" "run"
-
-REM all: "build" "run"
+)
