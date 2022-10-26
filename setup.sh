9fed965bee0276cde612740fc4d2d8fb3b1373c1
Brian West <brian@signalwire.com>
2022-10-26 09:57:26 -0500

    Update setup.sh to ask for more

--- setup.sh
+++ setup.sh
@@ -6,9 +6,12 @@ if [ ! -f ".env" ]; then
    read -p "What is your Signalwire project ID: " proj_id;
    read -p "What is your Signalwire REST API token: " api_token;
    read -p "What is your NGROK Token: " ngrok_token;
-   read -p "Editor (nano, vim, emacs: " visual;
+   read -p "What is your LocaltoNet API TOKEN: " localtonet_api_token;
+   read -p "What is your LocaltoNet AUTH TOKEN: " localtonet_auth_token;
+   read -p "What is your Work Dir: " workdir;
+   read -p "Editor (nano, vim, emacs): " visual;
 
-   URL="${sig_space}/api/laml/2010-04-01/Accounts -u ${proj_id}:${api_token}"
+   URL="https://${sig_space}.signalwire.com/api/laml/2010-04-01/Accounts -u ${proj_id}:${api_token}"
    response_code=$(curl -s -o /dev/null -I -w "%{http_code}" $URL )
    if [[ $response_code  -eq 200 ]]; then
       echo "SIGNALWIRE_SPACE=$sig_space" > .env
@@ -16,6 +19,9 @@ if [ ! -f ".env" ]; then
       echo "REST_API_TOKEN=$api_token" >> .env
       echo "NGROK_TOKEN=$ngrok_token" >> .env
       echo "VISUAL=$visual" >> .env
+      echo "LOCALTONET_API_TOKEN=$localtonet_api_token"
+      echo "LOCALTONET_AUTH_TOKEN=$localtonet_auth_token" >> .env
+      echo "WORKDIR=$workdir" >> .env
       echo "setup successful"
    elif [[ $response_code -eq 404 ]]; then
       echo "Make sure you entered correct space URL"
