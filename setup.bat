31470adec15b6cff84f2b5c208136297f72042b3
Len <Len-PGH@users.noreply.github.com>
2022-10-26 14:26:32 -0400

    Update setup.bat (#39)
    * Update setup.bat
    
    * Add prompt for work dir

--- setup.bat
+++ setup.bat
@@ -15,8 +15,9 @@ set /p proj_id="What is your Signalwire Project ID "
 set /p api_token="What is your Signalwire REST API token "
 set /p ngrok_token="What is your NGROK Token (Optional) "
 set /p visual_editor="What editor to use? nano, vim, emacs "
-set /p localtonet_api_token="What is your localtonet API Token "
+set /p localtonet_api_token="What is your localtonet API Token (Optional) "
 set /p localtonet_auth_token="What is your localtonet Tunnel Token (Optional) "
+set /p work_dir="Define work directory, C:/SWISH "
 
 echo SIGNALWIRE_SPACE=%sig_space%> .env
 echo PROJECT_ID=%proj_id%>> .env
@@ -25,7 +26,7 @@ echo NGROK_TOKEN=%ngrok_token%>> .env
 echo VISUAL=%visual_editor%>> .env
 echo LOCALTONET_API_TOKEN=%localtonet_api_token%>> .env
 echo LOCALTONET_AUTH_TOKEN=%localtonet_auth_token%>> .env
-echo WORKDIR=/tmp>> .env
+echo WORKDIR=%work_dir%>> .env
 
 REM CD docker
 docker network create --attachable wirestarter --subnet 172.50.0.1/24
