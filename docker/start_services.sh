89e1a92d496fba4f4619a4c800b4f2b64df755b9
Brian West <brian@signalwire.com>
2022-11-03 15:11:09 -0500

    Clean up start services

--- docker/start_services.sh
+++ docker/start_services.sh
@@ -6,33 +6,32 @@
 
 # Start Ngrok in a screen
 if [ ! -z $NGROK_TOKEN ]; then
-    /usr/local/bin/ngrok config add-authtoken $NGROK_TOKEN
-    /usr/bin/screen -dmS ngrok /usr/local/bin/ngrok --log=/root/ngrok.log http 9080
-    sleep 3  # wait for the tunnel to start #
-    NGROK_URL=$( grep url /root/ngrok.log | cut -d= -f 8 | sed 's/https:\/\///g' | tail -1 )
-    echo $NGROK_URL > /root/ngrok.url
-    # Run python script to update the numbers
-    # Pass in ngrok_url as an arg
-    if [ ! -z $NGROK_URL ]; then
-	python3 /usr/lib/cgi-bin/update_laml_bins.py $NGROK_URL
-    fi
+    /usr/local/bin/ngrok config add-authtoken $NGROK_TOKEN > /dev/null 2>&1
+    /usr/bin/screen -dmS ngrok /usr/local/bin/ngrok --log=/var/log/ngrok.log http 9080
 fi
 
 # Start LocalToNet in screen
 if [ ! -z $LOCALTONET_AUTH_TOKEN ]; then
     /usr/bin/screen -dmS localtonet /usr/bin/localtonet authtoken $LOCALTONET_AUTH_TOKEN
-    sleep 3
-    if [ ! -z $LOCALTONET_API_TOKEN ]; then
-	curl -s -o -  -X GET https://localtonet.com/api/GetTunnelsByAuthToken/$LOCALTONET_AUTH_TOKEN -H 'accept: */*' -H "Authorization: Bearer $LOCALTONET_API_TOKEN" | jq -r '.result[].url' | \
-	    sed 's/https:\/\///g' | head -1 > /root/localtonet.url
-    fi
 fi
+
+sleep 2
 
 # Loop so we can update the urls if they change while running.
 while true
 do
-    export NGROK_URL=`cat /root/ngrok.url`;
-    export LOCALTONET_URL=`cat /root/localtonet.url`;
+    if [ ! -z $NGROK_TOKEN ]; then
+	export NGROK_URL=$( curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url' )
+	echo $NGROK_URL > /root/ngrok.url
+    fi
+    if [ ! -z $NGROK_URL ]; then
+	# Run python script to update the numbers
+	# Pass in ngrok_url as an arg
+	python3 /usr/lib/cgi-bin/update_laml_bins.py $NGROK_URL
+    fi
+    if [ ! -z $LOCALTONET_API_TOKEN ]; then
+	export LOCALTONET_URL=$( curl -s -o -  -X GET https://localtonet.com/api/GetTunnelsByAuthToken/$LOCALTONET_AUTH_TOKEN -H 'accept: */*' -H "Authorization: Bearer $LOCALTONET_API_TOKEN" | jq -r '.result[].url' | head -1 )
+    fi
 
     clear
     echo -e "\n\n";
@@ -42,12 +41,12 @@ do
     echo -e "\n"
 
     if [ ! -z $NGROK_URL ]; then
-	echo -e "NGROK Tunnel: https://$NGROK_URL";
-	echo -e "/workdir/public -> https://$NGROK_URL/public\n";
+	echo -e "NGROK Tunnel: $NGROK_URL";
+	echo -e "/workdir/public -> $NGROK_URL/public\n";
     fi
     if [ ! -z $LOCALTONET_URL ]; then
-	echo "LocalToNet Tunnel: https://$LOCALTONET_URL";
-	echo -e "/workdir/public -> https://$LOCALTONET_URL/public\n";
+	echo "LocalToNet Tunnel: $LOCALTONET_URL";
+	echo -e "/workdir/public -> $LOCALTONET_URL/public\n";
     fi
     if [ ! -z $WORKDIR ]; then
 	echo -e "Persistent host directory is /workdir -> $WORKDIR\n";
